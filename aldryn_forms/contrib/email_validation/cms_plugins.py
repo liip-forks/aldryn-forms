57897530e6b7f17878e82d2b19cfae8e92215caa
Raphaël Snts <r@0x73.ch>
2018-02-16 11:08:59 +0100

    Added a function to send a validation link to submiter

--- aldryn_forms/contrib/email_validation/cms_plugins.py
+++ aldryn_forms/contrib/email_validation/cms_plugins.py
@@ -6,6 +6,7 @@ from aldryn_forms.cms_plugins import FormPlugin
 from aldryn_forms.helpers import get_user_name
 from aldryn_forms.validators import is_valid_recipient
 from cms.plugin_pool import plugin_pool
+from emailit.api import send_mail
 
 from .forms import EmailValidationFormSubmissionBaseForm
 from .models import EmailValidationFormPlugin
@@ -19,6 +20,7 @@ class EmailValidationForm(FormPlugin):
 
     def form_valid(self, instance, request, form):
         form.instance.set_recipients(self.get_recipients(instance, form))
+        self.send_validation_email(instance, form)
         form.save()
 
     def get_recipients(self, instance, form):
@@ -41,6 +43,30 @@ class EmailValidationForm(FormPlugin):
             ('AldrynDynamicForm', (EmailValidationFormSubmissionBaseForm,), fields)
         )
         return formClass
+
+    def send_validation_email(self, instance, form):
+
+        email_fields = [(name, field) for name, field in form.fields.items()
+                       if field.__class__.__name__ == 'EmailField']
+        if len(email_fields) == 0:
+            return
+
+        email_field_name = email_fields[0][0]
+        email = form.data[email_field_name]
+
+        context = {
+            'form_name': instance.name,
+            'form_data': form.get_serialized_field_choices(),
+            'form_plugin': instance,
+            'validation_link': 'TODO: Generate link',
+        }
+
+        send_mail(
+            recipients=email,
+            context=context,
+            template_base='email_validation/emails/validation',
+            language=instance.language
+        )
 
 
 plugin_pool.register_plugin(EmailValidationForm)
