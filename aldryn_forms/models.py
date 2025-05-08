fde95b92ecec42491828ef0600662061fbaf68d8
Wolfgang Fehr <24782511+wfehr@users.noreply.github.com>
2025-05-08 09:49:00 +0200

    BaseFormPlugin.action_backend: avoid migrations from settings-changes
    Changing settings.ALDRYN_FORMS_ACTION_BACKENDS would result in new migrations.
    We can avoid this by setting the choices via 'FormPluginForm'.

--- aldryn_forms/models.py
+++ aldryn_forms/models.py
@@ -23,7 +23,7 @@ from .constants import WEBHOOK_METHODS
 from .fields import AldrynFormsLinkField
 from .helpers import is_form_element
 from .sizefield.models import FileSizeField
-from .utils import ALDRYN_FORMS_ACTION_BACKEND_KEY_MAX_SIZE, action_backend_choices, get_action_backends
+from .utils import ALDRYN_FORMS_ACTION_BACKEND_KEY_MAX_SIZE, get_action_backends
 
 
 AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
@@ -157,7 +157,6 @@ class BaseFormPlugin(CMSPlugin):
         verbose_name=_('Action backend'),
         max_length=ALDRYN_FORMS_ACTION_BACKEND_KEY_MAX_SIZE,
         default='default',
-        choices=action_backend_choices(),
     )
     webhooks = models.ManyToManyField(Webhook, blank=True)
 
